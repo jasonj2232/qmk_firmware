a66787a79aa2ea3fb1126e3a67f7846dca597680
Jason Devadoss <jasonj2232@gmail.com>
2023-11-01 09:42:23 +0530

    Added initial config for oled in rules.mk

--- keyboards/argo_works/sitara/mk0/avr/rules.mk
+++ keyboards/argo_works/sitara/mk0/avr/rules.mk
@@ -1,4 +1,6 @@
 NRKO_ENABLE = yes
+
+# Encoder
 ENCODER_MAP_ENABLE = yes
 
 # Audio
@@ -7,5 +9,10 @@ AUDIO_DRIVER = pwm_hardware
 # Haptics
 HAPTIC_ENABLE = yes
 HAPTIC_DRIVER = drv2605l
+
+# OLED
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
 
 LTO_ENABLE = yes
